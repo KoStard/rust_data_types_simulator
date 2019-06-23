32c3df2f99f8f46ce476a69681cbe188951fe764
KoStard <rub099999720@gmail.com>
2019-06-23 15:16:40 +0400

    Adding in and post order traversals.

--- src/algorithms/b_tree_traversals.rs
+++ src/algorithms/b_tree_traversals.rs
@@ -1,5 +1,6 @@
 use crate::data_structures::binary_tree::BinaryTree;
 
+#[allow(dead_code)]
 fn pre_order_traversal_util<T>(node: &BinaryTree<T>, vec: &mut Vec<T>) where T: Clone{
     vec.push(node.value.clone());
     if let Some(n) = &node.left {
@@ -10,9 +11,46 @@ fn pre_order_traversal_util<T>(node: &BinaryTree<T>, vec: &mut Vec<T>) where T: 
     }
 }
 
+#[allow(dead_code)]
 pub fn pre_order_traversal<T>(root: &BinaryTree<T>) -> Vec<T> where T: Clone{
     let mut res = Vec::new();
     pre_order_traversal_util(root, &mut res);
+    res
+}
+
+#[allow(dead_code)]
+fn in_order_traversal_util<T>(node: &BinaryTree<T>, vec: &mut Vec<T>) where T: Clone{
+    if let Some(n) = &node.left {
+        in_order_traversal_util(n, vec);
+    }
+    vec.push(node.value.clone());
+    if let Some(n) = &node.right {
+        in_order_traversal_util(n, vec);
+    }
+}
+
+#[allow(dead_code)]
+pub fn in_order_traversal<T>(root: &BinaryTree<T>) -> Vec<T> where T: Clone{
+    let mut res = Vec::new();
+    in_order_traversal_util(root, &mut res);
+    res
+}
+
+#[allow(dead_code)]
+fn post_order_traversal_util<T>(node: &BinaryTree<T>, vec: &mut Vec<T>) where T: Clone{
+    if let Some(n) = &node.left {
+        post_order_traversal_util(n, vec);
+    }
+    if let Some(n) = &node.right {
+        post_order_traversal_util(n, vec);
+    }
+    vec.push(node.value.clone());
+}
+
+#[allow(dead_code)]
+pub fn post_order_traversal<T>(root: &BinaryTree<T>) -> Vec<T> where T: Clone{
+    let mut res = Vec::new();
+    post_order_traversal_util(root, &mut res);
     res
 }
 
@@ -28,7 +66,6 @@ pub fn demo() {
     root.right = Some(Box::new(r));
     println!(
         "{:?}",
-        pre_order_traversal(&root)
+        post_order_traversal(&root)
     );
 }
-
